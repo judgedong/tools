a912682a77ad760a7287b612c77666b2d62c333f
judgedong <dqqsw@126.com>
2016-04-07 10:48:25 +0800

    Create Md5Util.java

---
+++ Md5Util.java
@@ -0,0 +1,52 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import sun.misc.BASE64Encoder;
+
+public class Md5Util {
+
+	/**
+	 * Md5+Base64进行加密
+	 * @param oldStr
+	 * @return
+	 */
+	public static String encode(String oldStr){
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("md5");
+			byte []afterStr = md5.digest(oldStr.getBytes());
+			
+			BASE64Encoder b64 = new BASE64Encoder();
+			return b64.encode(afterStr);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	/**
+	 * Mysql,Oracle的md5函数加密
+	 * @param oldStr
+	 * @return
+	 */
+	public static String DBEncode(String oldStr){
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("md5");
+			byte []afterStr = md5.digest(oldStr.getBytes());
+			
+			BigInteger bigInt = new BigInteger(1, afterStr);
+			return bigInt.toString(16);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(encode("123"));
+		System.out.println(DBEncode("1"));
+		
+	}
+	
+}
